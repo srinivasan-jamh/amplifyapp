53e70c4370818e33d837f817618fd7db87c1f71f
srinivasan-jamh <srinivasan.dev@jamh.ai>
2021-06-02 12:13:29 -0600

    V2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Srinivasan Sivalingam</h1>
+        <h1>Hello from Srinivasan Sivalingam V2</h1>
       </header>
     </div>
   );
